d63a5a7d422b0fa826fe694bdb97c8371ed23dde
admin <admin@bespin.local>
2026-06-23 22:53:35 -0400

    Redesign icons: add copy, download, star, dots

---
+++ frontend-overrides/js/__tests__/redesign-icons.test.js
@@ -0,0 +1,17 @@
+import { test } from 'node:test';
+import assert from 'node:assert';
+import { I } from '../redesign/icons.js';
+
+test('copy and download icons return svg strings', () => {
+  assert.match(I.copy(), /<svg[\s\S]*<\/svg>/);
+  assert.match(I.download(), /<svg[\s\S]*<\/svg>/);
+});
+
+test('dots icon returns an svg', () => {
+  assert.match(I.dots(), /<svg[\s\S]*<\/svg>/);
+});
+
+test('star is hollow by default and filled when requested', () => {
+  assert.match(I.star(13, false), /fill="none"/);
+  assert.match(I.star(13, true), /fill="currentColor"/);
+});
